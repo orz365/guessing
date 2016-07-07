fbbd3f7662544ffac7b40423efa7e1336b74b138
orz365 <orz365@163.com>
2016-07-07 16:37:22 +0800

    test

---
+++ js/moin.js
@@ -0,0 +1,44 @@
+$(function(){
+	
+	$.fn.moinProgress  = function(){
+		var $this = $(this);
+		var rate = $this.find('.moin-progress-rate');
+		var rate_value = $this.data('rate');
+		rate.width(rate_value);
+	}
+	
+	$('.moin-progress-bar').moinProgress(function(){
+		
+	});
+	
+	
+	//绑定事件
+	$('.moin-history-back').on('click',function(e){
+		window.history.back();
+	})
+	
+	$('.moin-index-checkin-btn').on('click',function(){
+		window.location.href="checkin.html";
+	})
+	$('.ui-icon-home').on('click',function(e){
+		window.location.href="index.html";
+	})
+	$('.moin-check-btn').on('click',function(e){
+		$(this).attr('disabled','disabled');
+		window.location.href="checkin.html";
+	})
+	
+	
+	$('.moin-index-login-btn').on('click',function(e){
+		window.location.href="login.html";
+	})
+	//我的魔豆
+	$('.moin-mygold-btn').on('click',function(){
+		window.location.href="mygold.html";
+	})
+	   
+	//
+	$('#login_btn').on('click',function(e){
+		window.location.href="index.html";
+	})
+})
