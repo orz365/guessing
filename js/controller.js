256ff2bc62279456df7c1b03dea5de0e14d8d3bc
orz365 <orz365@163.com>
2016-07-08 18:11:57 +0800

    2016-7-8

---
+++ js/controller.js
@@ -0,0 +1,15 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('myCtrl',function($scope,$http){
+	$scope.phones = [{
+		name:'10'
+	},
+	{
+		name:'10'
+	},
+	{
+		name:'10'
+	}
+	
+	];
+});
+ 
